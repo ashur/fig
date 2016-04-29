0a35f010f271dbde57e5627a8d77ce4cb5631518
Ashur Cabrera <hello@cabreramade.co>
2016-04-28 17:44:26 -0700

    adding rudimentary 'deploy' and 'run' commands

---
+++ lib/commands/_global.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * This file is part of Fig
+ */
+namespace Fig;
+
+use \Huxtable\CLI;
+use \Huxtable\Core\File;
+
+$dirFig = new File\Directory( getenv( 'HOME' ) . '/.fig' );
+$fig = new Fig( $dirFig );
+
+/**
+ * @param	string	$query			ex., "<app>/<profile>" or "<app>:<command>"
+ * @param	string	$delimeter		ex., "/" or ":"
+ * @param	array	$labels			ex., ['app','profile']
+ * @return	array
+ */
+function parseQuery( $query, $delimiter, array $labels )
+{
+	$params = [];
+	$queryPieces = explode( $delimiter, $query );
+
+	$expectedPiecesCount = count( $labels );
+	if( count( $queryPieces ) == $expectedPiecesCount )
+	{
+		for( $i = 0; $i < $expectedPiecesCount; $i++ )
+		{
+			if( empty( $queryPieces[$i] ) )
+			{
+				throw new CLI\Command\IncorrectUsageException();
+			}
+
+			$params[$labels[$i]] = $queryPieces[$i];
+		}
+
+		return $params;
+	}
+
+	throw new CLI\Command\IncorrectUsageException();
+}
+
+/*
+ * Commands
+ */
+/**
+ * @command		deploy
+ * @desc		Deploy a profile (an alias for 'command run')
+ * @usage		deploy <app>/<profile>
+ */
+$commandDeploy = new CLI\Command( 'deploy', 'Deploy a profile', function( $query )
+{
+	GLOBAL $fig;
+
+	try
+	{
+		$params = parseQuery( $query, '/', ['app','profile'] );
+		$fig->deployProfile( $params['app'], $params['profile'] );
+	}
+	catch( CLI\Command\IncorrectUsageException $e )
+	{
+		throw new CLI\Command\IncorrectUsageException( $this->getUsage(), 1 );
+	}
+	catch( \Exception $e )
+	{
+		throw new CLI\Command\CommandInvokedException( $e->getMessage(), 1 );
+	}
+});
+
+$usageDeploy = "deploy <app>/<profile>";
+$commandDeploy->setUsage( $usageDeploy );
+
+/**
+ * @command		run
+ * @desc		Run a command (an alias for 'command run')
+ * @usage		run <app>:<command>
+ */
+$commandRun = new CLI\Command( 'run', 'Run a command', function( $query )
+{
+	GLOBAL $fig;
+
+	try
+	{
+		$params = parseQuery( $query, ':', ['app','command'] );
+		$fig->executeCommand( $params['app'], $params['command'] );
+	}
+	catch( CLI\Command\IncorrectUsageException $e )
+	{
+		throw new CLI\Command\IncorrectUsageException( $this->getUsage(), 1 );
+	}
+	catch( \Exception $e )
+	{
+		throw new CLI\Command\CommandInvokedException( $e->getMessage(), 1 );
+	}
+});
+
+$usageRun = "run <app>:<command>";
+$commandRun->setUsage( $usageRun );
---
+++ lib/commands/deploy.php
@@ -0,0 +1,13 @@
+<?php
+
+use Huxtable\CLI;
+use Huxtable\CLI\Input;
+
+/**
+ * @command		deploy
+ * @desc		Deploy a profile (an alias for 'command run')
+ * @usage		deploy <app>/<profile>
+ */
+
+// Defined in _global
+return $commandDeploy;
