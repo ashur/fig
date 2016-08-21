a3768f0d17b02b0deb01354502204740321b191f
Ashur Cabrera <hello@cabreramade.co>
2016-08-21 13:07:57 -0700

    moved app listing (back) to global

--- lib/commands/_global.php
+++ lib/commands/_global.php
@@ -9,6 +9,66 @@ use \Huxtable\CLI;
 use \Huxtable\Core\File;
 
 /**
+ * @param	array	$apps
+ * @return	Huxtable\CLI\Output
+ */
+function listApps( array $apps )
+{
+	$output = new CLI\Output;
+
+	if( count( $apps ) == 0 )
+	{
+		$output->line( "fig: No apps found. See 'fig add'." );
+	}
+
+	$formattedString = new CLI\Format\String;
+
+	if( !empty( $apps ) )
+	{
+		$padTop = false;
+		foreach( $apps as $app )
+		{
+			if( $padTop)
+			{
+				$output->line();
+			}
+
+			/* App Name */
+			$formattedString->foregroundColor( 'green' );
+			$formattedString->setString( $app->getName() . '/' );
+			$output->line( $formattedString );
+
+			/* Profiles */
+			$formattedString->foregroundColor( 'cyan' );
+			$profiles = $app->getProfiles();
+
+			foreach( $profiles as $profile )
+			{
+				$profileName = $profile->getName();
+				$profileSummary = "  {$profileName}";
+
+				/* Extending profiles */
+				$extendsProfile = $profile->getParentName();
+				if( !is_null( $extendsProfile ) )
+				{
+					$stringProfile = new CLI\Format\String;
+					$stringProfile->foregroundColor( 'purple' );
+					$stringProfile->setString( $profileName );
+
+					$profileSummary = "  {$stringProfile} -> {$extendsProfile}";
+				}
+
+				$output->line( $profileSummary );
+			}
+
+			$padTop = true;
+		}
+	}
+
+	return $output;
+}
+
+/**
  * @param	string	$query			ex., "<app>/<profile>" or "<app>:<command>"
  * @param	string	$delimeter		ex., "/" or ":"
  * @param	array	$labels			ex., ['app','profile']
--- lib/commands/show.php
+++ lib/commands/show.php
@@ -9,66 +9,6 @@ use Huxtable\CLI;
 use Huxtable\CLI\Command;
 
 /**
- * @param	array	$apps
- * @return	Huxtable\CLI\Output
- */
-function listApps( array $apps )
-{
-	$output = new CLI\Output;
-
-	if( count( $apps ) == 0 )
-	{
-		$output->line( "fig: No apps found. See 'fig create'." );
-	}
-
-	$formattedString = new CLI\Format\String;
-
-	if( !empty( $apps ) )
-	{
-		$padTop = false;
-		foreach( $apps as $app )
-		{
-			if( $padTop)
-			{
-				$output->line();
-			}
-
-			/* App Name */
-			$formattedString->foregroundColor( 'green' );
-			$formattedString->setString( $app->getName() . '/' );
-			$output->line( $formattedString );
-
-			/* Profiles */
-			$formattedString->foregroundColor( 'cyan' );
-			$profiles = $app->getProfiles();
-
-			foreach( $profiles as $profile )
-			{
-				$profileName = $profile->getName();
-				$profileSummary = "  {$profileName}";
-
-				/* Extending profiles */
-				$extendsProfile = $profile->getParentName();
-				if( !is_null( $extendsProfile ) )
-				{
-					$stringProfile = new CLI\Format\String;
-					$stringProfile->foregroundColor( 'purple' );
-					$stringProfile->setString( $profileName );
-
-					$profileSummary = "  {$stringProfile} -> {$extendsProfile}";
-				}
-
-				$output->line( $profileSummary );
-			}
-
-			$padTop = true;
-		}
-	}
-
-	return $output;
-}
-
-/**
  * @command		app
  * @desc		List apps and their profiles
  * @usage		app
