6111492fcbb8fb126ed88e8e8ec6bff0bb5ea2cc
Ashur Cabrera <hello@cabreramade.co>
2017-10-02 21:54:39 -0700

    🔥 Removing unneeded helper files

--- lib/helpers/application.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Cranberry\Core\File;
-
-/*
- * Set data directory
- */
-$dataDirectory = new File\Directory( '~/.fig' );
-$app->setDataDirectory( $dataDirectory );
--- lib/helpers/fig.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * Set data directory
- */
-$fig = new Fig\Fig( $dataDirectory );
-$app->registerCommandObject( 'fig', $fig );
