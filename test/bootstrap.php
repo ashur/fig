16e59a5a9f5f01936c848af86c76e0c02a69add2
Ashur Cabrera <hello@cabreramade.co>
2017-10-26 15:03:33 +0000

    🔧 Updating PHPUnit config files

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,14 +1,9 @@
 <?php
+
 /*
  * This file is part of Fig
  */
 
 $projectDir = dirname( __DIR__ );
 
-/* Fig autoloading */
-require_once( "{$projectDir}/lib/src/Fig/Autoloader.php" );
-Autoloader::register();
-
-/* Vendor autoloading */
-include_once( "{$projectDir}/vendor/cranberry/cli/autoload.php" );
-include_once( "{$projectDir}/vendor/spyc/Spyc.php" );
+require_once( "{$projectDir}/vendor/autoload.php" );
