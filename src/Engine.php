b21456454be7f02363804969516467c85a234ffb
Ashur Cabrera <hello@cabreramade.co>
2017-10-29 15:55:29 -0700

    ✨ Adding command execution via Engine

---
+++ src/Engine.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of Fig
+ */
+namespace Fig;
+
+class Engine
+{
+	/**
+	 * Returns whether a command exists on the host system
+	 *
+	 * @param	string	$command
+	 *
+	 * @return	bool
+	 */
+	public function commandExists( string $command ) : bool
+	{
+		$result = $this->executeCommand( 'which', [$command] );
+		return $result['exitCode'] == 0;
+	}
+
+	/**
+	 * Executes a command
+	 *
+	 * Returns an array ['output' => <array>, 'exitCode' => <int>]
+	 *
+	 * @param	string	$command
+	 *
+	 * @param	array	$arguments
+	 *
+	 * @return	array
+	 */
+	public function executeCommand( string $command, array $arguments=[] ) : array
+	{
+		$commandPieces = $arguments;
+		array_unshift( $commandPieces, $command );
+
+		foreach( $commandPieces as &$commandPiece )
+		{
+			$commandPiece = escapeshellarg( $commandPiece );
+		}
+
+		$commandString = implode( ' ', $commandPieces );
+		exec( $commandString, $output, $exitCode );
+
+		return [
+			'output' => $output,
+			'exitCode' => $exitCode
+		];
+	}
+}
---
+++ test/EngineTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Fig
+ */
+namespace Fig;
+
+use PHPUnit\Framework\TestCase;
+
+class EngineTest extends TestCase
+{
+	public function test_executeCommand_returnsArray()
+	{
+		$engine = new Engine();
+		$result = $engine->executeCommand( 'echo', ['hello'] );
+
+		$this->assertTrue( is_array( $result ) );
+		$this->assertArrayHasKey( 'output', $result );
+		$this->assertContains( 'hello', $result['output'] );
+
+		$this->assertArrayHasKey( 'exitCode', $result );
+		$this->assertEquals( 0, $result['exitCode'] );
+	}
+}
