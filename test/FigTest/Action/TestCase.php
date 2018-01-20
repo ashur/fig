2d232befae44cdfbcefc9b834da5c85dc767c720
Ashur Cabrera <hello@cabreramade.co>
2018-01-19 21:44:12 -0800

    ✅ Updating Action\TestCase
    adding getName to all Action test cases

--- test/FigTest/Action/TestCase.php
+++ test/FigTest/Action/TestCase.php
@@ -9,12 +9,36 @@ use Fig\Action\AbstractAction;
 
 abstract class TestCase extends \FigTest\TestCase
 {
+	/* Helpers */
+	abstract public function createObject_fromName( string $name ) : AbstractAction;
+
 	/* Providers */
 	abstract public function provider_ActionObject() : array;
 
 	/* Tests */
-	abstract public function test_getName();
 	abstract public function test_getSubtitle();
+
+	public function test_getName()
+	{
+		$name = getUniqueString( 'my action ' );
+		$action = $this->createObject_fromName( $name );
+
+		$this->assertEquals( $name, $action->getName() );
+	}
+
+	public function test_getName_withVariableReplacement()
+	{
+		$time = time();
+
+		$pattern = 'my action %s';
+		$nameString = sprintf( $pattern, '{{ time }}' );
+		$expectedName = sprintf( $pattern, $time );
+
+		$action = $this->createObject_fromName( $nameString );
+		$action->setVariables( ['time' => $time] );
+
+		$this->assertEquals( $expectedName, $action->getName() );
+	}
 
 	/**
 	 * @dataProvider	provider_ActionObject
