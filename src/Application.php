2b1a67445474fe52840f9eccea20a32524b4142f
Ashur Cabrera <hello@cabreramade.co>
2018-02-10 16:01:26 -0800

    🔨 Adding repository management to Application

--- src/Application.php
+++ src/Application.php
@@ -10,4 +10,34 @@ class Application
 	const NAME    = 'fig';
 	const VERSION = '0.5a';
 	const PHP_MIN = '7.0';
+
+	/**
+	 * @var	array
+	 */
+	protected $repositories=[];
+
+	/**
+	 * Adds a Repository object
+	 *
+	 * @param	Fig\Repository	$repository
+	 *
+	 * @return	void
+	 */
+	public function addRepository( Repository $repository )
+	{
+		$repositoryName = $repository->getName();
+		$this->repositories[$repositoryName] = $repository;
+	}
+
+	/**
+	 * Returns whether repository is defined
+	 *
+	 * @param	string	$repositoryName
+	 *
+	 * @return	bool
+	 */
+	public function hasRepository( string $repositoryName ) : bool
+	{
+		return isset( $this->repositories[$repositoryName] );
+	}
 }
--- test/Fig/ApplicationTest.php
+++ test/Fig/ApplicationTest.php
@@ -5,10 +5,37 @@
  */
 namespace Fig;
 
-use PHPUnit\Framework\TestCase;
+use FigTest\TestCase;
 
 class ApplicationTest extends TestCase
 {
+	/* Helpers */
+
+	public function createObject()
+	{
+		$application = new Application();
+		return $application;
+	}
+
+	/* Providers */
+
+
+	/* Tests */
+
+	public function test_addRepository()
+	{
+		$application = $this->createObject();
+
+		$repoName = getUniqueString( 'repo' );
+		$repository = new Repository( $repoName );
+
+		$this->assertFalse( $application->hasRepository( $repoName ) );
+
+		$application->addRepository( $repository );
+
+		$this->assertTrue( $application->hasRepository( $repoName ) );
+	}
+
 	public function test_const_NAME()
 	{
 		$this->assertTrue( is_string( Application::NAME ) );
@@ -24,5 +51,13 @@ class ApplicationTest extends TestCase
 	{
 		$this->assertTrue( is_string( Application::PHP_MIN ) );
 		$this->assertTrue( version_compare( Application::PHP_MIN, '0.0', '>=' ) );
+	}
+
+	public function test_hasRepository()
+	{
+		$application = $this->createObject();
+		$repoName = getUniqueString( 'repo' );
+
+		$this->assertFalse( $application->hasRepository( $repoName ) );
 	}
 }
