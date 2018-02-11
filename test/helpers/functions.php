63f9cdb421b7b80ba3626abd1720ca406e1d3a68
Ashur Cabrera <hello@cabreramade.co>
2018-02-10 17:16:45 -0800

    ✅ Adding new test helper function
    for quickly getting Directory object representing temporary directory in 'test' folder

--- test/helpers/functions.php
+++ test/helpers/functions.php
@@ -5,6 +5,27 @@
  */
 
 /**
+ * Returns an object representing a temporary directory for use during test
+ * execution.
+ *
+ * If the directory does not exist, it will be created automatically.
+ *
+ * @return	Cranberry\Filesystem\Directory
+ */
+function getTemporaryDirectory() : Cranberry\Filesystem\Directory
+{
+	$path = sprintf( '%s/tmp', dirname( __DIR__ ) );
+	$directory = new Cranberry\Filesystem\Directory( $path );
+
+	if( !$directory->exists() )
+	{
+		$directory->create();
+	}
+
+	return $directory;
+}
+
+/**
  * Returns a (mostly) unique string
  *
  * @param	string	$prefix
